29e1ff4f105436dd5c7ebfb0423bdda61b357a50
Cristian Arraya <arrayacristian75@gmail.com>
2024-04-08 15:00:43 -0300

    Se hizo una practica

--- hello.cpp
+++ hello.cpp
@@ -1,18 +1,30 @@
-// Tipos de datos basicos de C++
-#include <iostream>
+/*Escribre un programa que lea de la entrada de la terminal dos numero y muestre en la salida 
+de terminal su suma, resta, multiplicacion y division*/
 
+#include<iostream>
 using namespace std;
 
-int main()
-{
-  //Este mensaje es por que no me aparece los cambios subidos en github
-  //Es un numero completo, no lleva coma 
-  int entero = 15;
-  float flotante = 10.45; 
-  double mayor = 16.3456;
-  //Se solo un caracter
-  char letra = 'a';
-  bool booleano = true;
-  cout<<mayor;
+int main(){
+  int numero1;
+  int numero2;
+  int suma = 0;
+  int resta = 0;
+  int multiplicacion = 0;
+  int division = 0;
+
+  cout<<"Escribre un numero: ";
+  cin>> numero1;
+  cout<<"Escribre otro numero: ";
+  cin>>numero2;
+
+  suma = numero1 + numero2;
+  resta = numero1 - numero2;
+  multiplicacion = numero1 * numero2;
+  division = numero1 / numero2;
+
+  cout<<"\nLa suma es: " << suma<<endl;
+  cout<<"\nLa resta es: " << resta<<endl;
+  cout<<"\nLa multiplicacion es: " << multiplicacion<<endl;
+  cout<<"\nLa division es: " << division<<endl;
   return 0;
 }
