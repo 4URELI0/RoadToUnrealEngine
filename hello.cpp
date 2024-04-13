ff95cf5af12ccad9a97ef00943ded40551a2a8c4
Cristian Arraya <arrayacristian75@gmail.com>
2024-04-13 14:07:14 -0300

    Se realizo el ejecicio de calcular las notas de los alumno

--- hello.cpp
+++ hello.cpp
@@ -1,19 +1,30 @@
-/*Escribe un fragmento de programa que intercambie los valores entre dos variables*/
+/*
+La calificacion final de un estudidante es la media ponderada de 3 notas
+La nota de practica que es un 30%
+la nota de teorica que cuenta 60%
+la nota de participacion que es un 10%
+Escribe un programa que lea la entrada estandar. Las 3 notas del alumno y 
+escribe en la solida entrada su nota final.
+*/
 #include <iostream>
 using namespace std;
 
 int main(){
-  int x,y,aux;
-  cout<<"Escribe el valor de X: "; cin>>x;
-  cout<<"Escribe el valor de Y: "; cin>>y;
-
-  aux = x;//Auxiliar guarde el valor de X osea auxiliar va a valer X
-  x = y;//X va a guardar el valor de Y 
-  y = aux;//Y va a guarda el valor X
-  
-  cout<<"\n El nuevo valor de X es: "<<x<<endl;
-  cout<<"\n El nuevo valor de Y es: "<<y<<endl;
-
-
+  /*Declaramos las variables*/
+  float notaPractica, notaTeorica, notaParticipacion;
+  float notaFinal = 0;
+  /*Hacemos que el usuario escriba los datos*/
+  cout<<"Escribe la nota de practica: "; cin>>notaPractica;
+  cout<<"Escribe la nota de teorica: "; cin>>notaTeorica;
+  cout<<"Escribe la nota de participacion: "; cin>>notaParticipacion;
+  /*Realizamos las operaciones o calculos*/
+  notaPractica *=  0.30;
+  notaTeorica *= 0.60;
+  notaParticipacion *=  0.10;
+ 
+  notaFinal = notaPractica + notaTeorica + notaParticipacion;
+  /*Nota final del alumno*/
+  cout<<"\n La nota final es: "<<notaFinal;
+ 
   return 0;
 }
