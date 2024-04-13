0211aa42920e4763e182a404ab352101d3e51ce1
Cristian Arraya <arrayacristian75@gmail.com>
2024-04-13 00:31:59 -0300

    Se realizo la 2da practica de expresiones

--- hello.cpp
+++ hello.cpp
@@ -3,16 +3,22 @@
 using namespace std;
 
 int main(){
-  float a,b,c,d;
-  float resultado = 0;
-  cout<<"Escribe el valor de A: ";cin>>a;
-  cout<<"Escribe el valor de B: ";cin>>b;
-  cout<<"Escribe el valor de C: ";cin>>c;
-  cout<<"Escribe el valor de D: ";cin>>d;
-
-  resultado = (a+b) / (c+d);
+  /*Declaramos las variables*/
+  float a,b,c,d,e,f;
+  float resultado = 0;//Almacenara el valor del resultado
+  
+  /*El usuario va a ingresar los valores*/
+  cout<<"Escribe el valor A: "; cin>> a;
+  cout<<"Escribe el valor B: "; cin>> b;
+  cout<<"Escribe el valor C: "; cin>> c;
+  cout<<"Escribe el valor D: "; cin>> d;
+  cout<<"Escribe el valor E: "; cin>> e;
+  cout<<"Escribe el valor F: "; cin>> f;
+  /*Operacion que debe realizar el programa*/
+  resultado = (a + (b/c)) / (d + (e/f));
+  /*Nos ayduara a reducir la cantidad de numero al obtener el resultado osea lo va a redondear*/
   cout.precision(3);
-  cout<<"El resultado es: " <<resultado;
-
+  cout<<"\nEl resultado es: "<<resultado<<endl;
+  
   return 0;
 }
