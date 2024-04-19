f719eecc0af0ba95b3a805dcda3e4bb18bce277e
Cristian Arraya <arrayacristian75@gmail.com>
2024-04-19 20:22:18 -0300

    Realizar un cajero virtual con un saldo inicial de 1k de dolares y este tenga opciones de ingresar mas dinero, retirar o salir del banco

--- hello.cpp
+++ hello.cpp
@@ -1,26 +1,46 @@
-/*Mostrar los meses del anio, pidiendole al usuario un numero entre (1-12), y mostrar el mes al que corresponde*/
+/*Hacer un programa que simule un cajero automatico con un saldo inicial de 1k USD*/
 
- #include<iostream>
- using namespace std;
+#include <iostream>
+using namespace std;
 
- int main(){
- int numero;
-  cout<<"Ingrese un numero y le indicaremos el mes: "; cin>>numero;
-  switch (numero)
+int main()
+{
+  int saldoInicial = 1000, opcion;
+  float extra, saldoActual = 0, retiro;
+  /*Va a dejar espacio en la linea el "\t"*/
+  cout << "\tBienvenido a su cajero virtual" << endl;
+  cout << "1. Ingresar dinero en cuenta" << endl;
+  cout << "2. Retirar dinero de la cuenta" << endl;
+  cout << "3. Salir del cajero" << endl;
+  cout << "Opciones: ";
+  /*La opcion que ponga el usuario de valor lo va a iniciar*/
+  cin >> opcion;
+
+  switch (opcion)
   {
-  case 1: cout<<"Enero"; break;
-  case 2: cout<<"Febrero";break;
-  case 3: cout<<"Marzo";break;
-  case 4: cout<<"Abril";break;
-  case 5: cout<<"Mayo";break;
-  case 6: cout<<"Junio";break;
-  case 7: cout<<"Julio";break;
-  case 8: cout<<"Agosto";break;
-  case 9: cout<<"Septiembre";break;
-  case 10: cout<<"Octubre";break;
-  case 11: cout<<"Noviembre";break;
-  case 12: cout<<"Diciembre";break;
-  default: cout<<"El numero ingresado salio del rango requerido para mostrar el mes";break;
+  case 1:
+    cout << "Ingrese la cantidad de dinero que quiere ingresar: ";
+    cin >> extra;
+    saldoActual = extra + saldoInicial;
+    cout << "Dinero en su cuenta: " << saldoActual;
+    break;
+  case 2:
+    cout << "Escribe cuanto dinero quiere retirar: ";
+    cin >> retiro;
+    if (retiro > saldoInicial)
+    {
+      cout << "Su cuenta no tiene fondo suficientes para retirar el dinero deseado";
+    }
+    else
+    {
+      saldoActual = saldoInicial - retiro;
+      cout << "Cantidad de dinero retirado: " << retiro << "$" << endl;
+      cout << "Dinero actual de la cuenta: " << saldoActual << "$" << endl;
+    }
+    break;
+  case 3:
+    cout << "Gracias por confiar en nosotros, hasta luego.";
+    break;
   }
   return 0;
- }
+}
