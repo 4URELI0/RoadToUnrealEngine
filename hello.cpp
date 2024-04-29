314c16c3b25c44fc76e7f5a0dd8657a2068ec6f1
Cristian Arraya <arrayacristian75@gmail.com>
2024-04-28 23:00:16 -0300

    Se agrego mas comentario para entender a la logica de las lineas

--- hello.cpp
+++ hello.cpp
@@ -10,19 +10,21 @@ using namespace std;
 
 int main()
 {
-  int numbers[6];
+  int numbers[7];
   cout << "Introduce los elementos de un array: " << endl;
-  for (int i = 0; i < 5; i++)
+  for (int i = 0; i <= 6; i++)
   {
     cin >> numbers[i];
   }
   cout << "Elementos de un array son: " << endl;
-  for (int i = 0; i < 5; i++)
+  for (int i = 0; i <= 6; i++)
   {
     cout << numbers[i] << " ";
   }
+  /*El arreglo va a mostrar los valores en reversa por que iniciamos un numero menor a 7 debido a que el
+  arreglo inicia desde 0 hasta 6 y seria 7 valores*/
   cout << "\nElementos de un array en reversa son: " << endl;
-  for (int i = 5; i > 0; i--)
+  for (int i = 6; i >= 0; i--)
   {
     cout << numbers[i] << " ";
   }
