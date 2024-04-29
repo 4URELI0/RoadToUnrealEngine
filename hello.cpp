97f7796fefaccd4ee38c05b64106e16ba88d3453
Cristian Arraya <arrayacristian75@gmail.com>
2024-04-28 23:15:30 -0300

    El usuario pondra el valor de tamano del arreglo y despues ingresara valores donde se calculara cual es el numero mayor entre los valores que ingreso el usuario

--- hello.cpp
+++ hello.cpp
@@ -1,32 +1,33 @@
-/*Escribe un programa que defina un vector de numeros y muestre en la salida estandar
-el vector en orden inverso del ultimo al primero elemento*/
+/*Desarrolle un programa que lea la entrada estandar un vector de enteros y saber el mayor elemento de vector*/
 #include <iostream>
 // #include <stdlib.h>
 // #include <time.h>
 #include <conio.h>
 // #include <math.h>
-#include <algorithm>
+// #include <algorithm>
 using namespace std;
 
 int main()
 {
-  int numbers[7];
-  cout << "Introduce los elementos de un array: " << endl;
-  for (int i = 0; i <= 6; i++)
+  int numberInt[100], n, biggerNumber = 0; // n es para guardar el numero de elementos del arreglo
+  cout << "Digite el numero de elemento en el arreglo: ";
+  cin >> n;
+  for (int i = 0; i < n; i++)
   {
-    cin >> numbers[i];
+    cout << i + 1 << ". Digite un numero: ";
+    cin >> numberInt[i];
+    // 1 2 4 10 5
+    if (numberInt[i] > biggerNumber)
+    {
+      biggerNumber = numberInt[i]; // 1 es mayor que 0? si, entonces se le asigna el valor a biggerNumber,
+                                   // 2 es mayor que 1? si, entonces se le asigna el valor a biggerNumber,
+                                   // 4 mayor que 2? si, entonces se le asigna el valor a biggerNumber
+                                   // 10 es mayor que 4? si, entonces se le asigna el valor a biggerNumber
+                                   // biggerNumber = 10
+                                   // 5 es mayor que 10? no, entonces el numero 10 queda dentro del elemento
+    }
   }
-  cout << "Elementos de un array son: " << endl;
-  for (int i = 0; i <= 6; i++)
-  {
-    cout << numbers[i] << " ";
-  }
-  /*El arreglo va a mostrar los valores en reversa por que iniciamos un numero menor a 7 debido a que el
-  arreglo inicia desde 0 hasta 6 y seria 7 valores*/
-  cout << "\nElementos de un array en reversa son: " << endl;
-  for (int i = 6; i >= 0; i--)
-  {
-    cout << numbers[i] << " ";
-  }
+  cout << "\n El mayor elemento del arreglo es: " << biggerNumber << endl;
+  getch();
   return 0;
 }
