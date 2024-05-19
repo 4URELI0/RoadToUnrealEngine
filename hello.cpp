13167a2dff347f1b90018bf90e27088ac7daa669
Cristian Arraya <arrayacristian75@gmail.com>
2024-05-19 17:44:48 -0300

    Se puso en practica el parametro CONST de los metodos

--- hello.cpp
+++ hello.cpp
@@ -4,20 +4,22 @@
 // #include <time.h>
 #include <ctime>
 #include <conio.h>
-#include <math.h>
-// #include <algorithm>
+// #include <math.h>
+//  #include <algorithm>
 using namespace std;
+
+void imprimirInfo(const string nombre, const string apellido, const int edad);
 
 int main(int argc, char const *argv[])
 {
-  int numero1;
-  int numero2;
-  cout << "Escriba un valor que sera multiplicado por el siguiente valor que usted ingrese: " << endl;
-  cin >> numero1;
-  cout << "Escriba otro valor: " << endl;
-  cin >> numero2;
-  int multiplicacion = numero1 * numero2;
-  int elevado = pow(multiplicacion,3);
-  cout << "El resultado de la multiplicacion con una elevacion al cubo es: " << elevado << endl;
+  string nombre = "Aurelio", apellido = "Arraya";
+  int edad = 19;
+  imprimirInfo(nombre, apellido, edad);
   return 0;
+}
+void imprimirInfo(const string nombre, const string apellido, const int edad)
+{
+  cout << "Nombre: " << nombre << endl;
+  cout << "Apellido: " << apellido << endl;
+  cout << "Edad: " << edad << endl;
 }
