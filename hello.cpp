47842d90366ce2a28dbc591e00ceb1cb6f122938
Cristian Arraya <arrayacristian75@gmail.com>
2024-05-19 01:56:55 -0300

    Se esta probando una nueva version de git para evitar errores

--- hello.cpp
+++ hello.cpp
@@ -1,51 +1,23 @@
-/*Realize un programa que calcule la suma de dos matrices de 3*3*/
+/**/
 #include <iostream>
 // #include <stdlib.h>
 // #include <time.h>
 #include <ctime>
 #include <conio.h>
-// #include <math.h>
+#include <math.h>
 // #include <algorithm>
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-  int numeros1[3][3], numero2[3][3], sumaMatriz[3][3];
-  cout << "Matriz 1 \n";
-  for (int i = 0; i < 3; i++)
-  {
-    for (int j = 0; j < 3; j++)
-    {
-      cout << "[" << i << "][" << j << "]: ";
-      cin >> numeros1[i][j];
-    }
-  }
-  cout << "Matriz 2 \n";
-  for (int i = 0; i < 3; i++)
-  {
-    for (int j = 0; j < 3; j++)
-    {
-      cout << "[" << i << "][" << j << "]: ";
-      cin >> numero2[i][j];
-    }
-  }
-  /*Suma de matriz*/
-  for (int i = 0; i < 3; i++)
-  {
-    for (int j = 0; j < 3; j++)
-    {
-      sumaMatriz[i][j] = numeros1[i][j] + numero2[i][j];
-    }
-  }
-  cout << "Resultado de la suma de matrices\n";
-  /*Mostrar a la matriz sumada*/
-  for (int i = 0; i < 3; i++)
-  {
-    for (int j = 0; j < 3; j++)
-    {
-      cout << "|" << sumaMatriz[i][j] << "|";
-    }
-    cout << "\n";
-  }
+  int numero1;
+  int numero2;
+  cout << "Escriba un valor que sera multiplicado por el siguiente valor que usted ingrese: " << endl;
+  cin >> numero1;
+  cout << "Escriba otro valor: " << endl;
+  cin >> numero2;
+  int multiplicacion = numero1 * numero2;
+  int elevado = pow(multiplicacion,3);
+  cout << "El resultado de la multiplicacion con una elevacion al cubo es: " << elevado << endl;
   return 0;
 }
