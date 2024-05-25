7dcb160fe2059903a1b28337e3be17f532b83f5b
Cristian Arraya <arrayacristian75@gmail.com>
2024-05-25 13:38:00 -0300

    Se realizo una modificacion en las matrices, ahora el usuario elige cuanta cantidad de filas y columnas deben tener las matrices para realizar el producto matriz, si son cuadrada realizara la operacion

--- hello.cpp
+++ hello.cpp
@@ -1,52 +1,71 @@
 /*Realice un programa que calcule el producto de dos matrices cuadrada de 3 x 3 */
 #include <iostream>
 #include <conio.h>
+#include <cstdlib>
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-  int matriz1[3][3], matriz2[3][3], productoMatriz[3][3];
+  int matriz1[100][100], matriz2[100][100], productoMatriz[100][100], filas, columnas;
 
+  cout << "Introduce la cantidad de filas de la matriz: ";
+  cin >> filas;
+
+  cout << "Introduce la cantidad de columnas de la matriz: ";
+  cin >> columnas;
+
+  /*Ingresar los valores de la matriz 1*/
   cout << "Matriz 1 \n";
-  for (int i = 0; i < 3; i++)
+  for (int i = 0; i < filas; i++)
   {
-    for (int j = 0; j < 3; j++)
+    for (int j = 0; j < columnas; j++)
     {
       cout << "[" << i << "]" << "[" << j << "]: ";
       cin >> matriz1[i][j];
     }
   }
 
+  /*Ingresar los valores de la matriz 2*/
   cout << "Matriz 2 \n";
-  for (int i = 0; i < 3; i++)
+  for (int i = 0; i < filas; i++)
   {
-    for (int j = 0; j < 3; j++)
+    for (int j = 0; j < columnas; j++)
     {
       cout << "[" << i << "]" << "[" << j << "]: ";
       cin >> matriz2[i][j];
     }
   }
 
-  /*Producto de matrices*/
-  for (int i = 0; i < 3; i++)
+  if (filas == columnas)
   {
-    for (int j = 0; j < 3; j++)
+    cout << "Es una matriz cuadrada";
+    /*Producto de matrices*/
+    for (int i = 0; i < filas; i++)
     {
-      productoMatriz[i][j] = 0;
-      for (int k = 0; k < 3; k++)
+      for (int j = 0; j < columnas; j++)
       {
-        productoMatriz[i][j] += matriz1[i][k] * matriz2[k][j];
+        productoMatriz[i][j] = 0;
+        for (int k = 0; k < 3; k++)
+        {
+          productoMatriz[i][j] += matriz1[i][k] * matriz2[k][j];
+        }
       }
+      cout << "\n";
+    }
+
+    /*Resultado del producto matriz por la terminal*/
+    for (int i = 0; i < filas; i++)
+    {
+      for (int j = 0; j < columnas; j++)
+      {
+        cout << "|" << productoMatriz[i][j] << "|";
+      }
+      cout << "\n";
     }
   }
-  /*Resultado*/
-  for (int i = 0; i < 3; i++)
+  else
   {
-    for (int j = 0; j < 3; j++)
-    {
-      cout << "|" << productoMatriz[i][j] << "|";
-    }
-    cout << "\n";
+    cout << "No es una matriz cuadrada";
   }
   return 0;
 }
