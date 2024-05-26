412940130efc0ca1cc63f5cb618cd98f15624129
Cristian Arraya <arrayacristian75@gmail.com>
2024-05-25 21:33:38 -0300

    Se realizo un programa que pida al usuario que ingrese una cadena de caracteres que superen los 10 caracteres, de caso contrario no mostrara la cadena que escribio por la pantalla

--- hello.cpp
+++ hello.cpp
@@ -1,4 +1,6 @@
-/*Longitud de una cadena de caracteres - Funcion strLen()*/
+/*Hacer un programa que pida al usuario que digite una cadena de caracteres, luego verificar la
+longitud de la cadena, y si esta supera los 10 caracteres mostrarla en pantalla, caso contrario
+no mostrarla*/
 #include <iostream>
 #include <conio.h>
 #include <string.h>
@@ -6,10 +8,22 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-  char palabra[] = "hola como estas?";
+  char palabra[30];
   int longitud = 0; // Guardamos el valor que nos va a dar strLen
+
+  cout << "Por favor escribe una cadena de caracteres: ";
+  cin.getline(palabra, 30);
   longitud = strlen(palabra);
-  cout << "Numeros de elementos de la cadena es: " << longitud << endl;
+
+  if (longitud > 10)
+  {
+    cout << "La palabra es: " << palabra << endl;
+    cout << "La longitud es: " << longitud;
+  }
+  else
+  {
+    cout << "Es menor que el valor de 10";
+  }
   getch();
   return 0;
 }
