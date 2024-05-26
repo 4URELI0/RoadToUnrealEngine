1b73d04b5629cfe073e21fb6aad6f86522d03c9a
Cristian Arraya <arrayacristian75@gmail.com>
2024-05-25 21:08:14 -0300

    Se ejecuto una nueva funcion de la cadena de caracteres

--- hello.cpp
+++ hello.cpp
@@ -1,71 +1,22 @@
-/*Realice un programa que calcule el producto de dos matrices cuadrada de 3 x 3 */
+/*Cadena de caracteres*/
 #include <iostream>
 #include <conio.h>
-#include <cstdlib>
+#include <string.h>
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-  int matriz1[100][100], matriz2[100][100], productoMatriz[100][100], filas, columnas;
+  char nombre[20];
 
-  cout << "Introduce la cantidad de filas de la matriz: ";
-  cin >> filas;
+  cout << "Escribe tu nombre: ";
+  /*Son 3 parametros que va a recibir el cin.getLine*/
+  //Se le va a indicar el nombre donde se va a guardar
+  //Le indicamos cuantos espacio va a ocupar
+  //Y se va a terminar cuando le indiquemos un salto de linea
+  cin.getline(nombre, 20, '\n');
 
-  cout << "Introduce la cantidad de columnas de la matriz: ";
-  cin >> columnas;
+  cout << nombre << endl;
 
-  /*Ingresar los valores de la matriz 1*/
-  cout << "Matriz 1 \n";
-  for (int i = 0; i < filas; i++)
-  {
-    for (int j = 0; j < columnas; j++)
-    {
-      cout << "[" << i << "]" << "[" << j << "]: ";
-      cin >> matriz1[i][j];
-    }
-  }
-
-  /*Ingresar los valores de la matriz 2*/
-  cout << "Matriz 2 \n";
-  for (int i = 0; i < filas; i++)
-  {
-    for (int j = 0; j < columnas; j++)
-    {
-      cout << "[" << i << "]" << "[" << j << "]: ";
-      cin >> matriz2[i][j];
-    }
-  }
-
-  if (filas == columnas)
-  {
-    cout << "Es una matriz cuadrada";
-    /*Producto de matrices*/
-    for (int i = 0; i < filas; i++)
-    {
-      for (int j = 0; j < columnas; j++)
-      {
-        productoMatriz[i][j] = 0;
-        for (int k = 0; k < 3; k++)
-        {
-          productoMatriz[i][j] += matriz1[i][k] * matriz2[k][j];
-        }
-      }
-      cout << "\n";
-    }
-
-    /*Resultado del producto matriz por la terminal*/
-    for (int i = 0; i < filas; i++)
-    {
-      for (int j = 0; j < columnas; j++)
-      {
-        cout << "|" << productoMatriz[i][j] << "|";
-      }
-      cout << "\n";
-    }
-  }
-  else
-  {
-    cout << "No es una matriz cuadrada";
-  }
+  getch();
   return 0;
 }
