b627e8c5e43e19a22531f076e938f8ec54f8f46f
Cristian Arraya <arrayacristian75@gmail.com>
2024-05-25 21:45:28 -0300

    Se realizo un programa que solicita el nombre del usuario y lo copia en otro arreglo de caracteres

--- hello.cpp
+++ hello.cpp
@@ -1,4 +1,5 @@
-/*Copiar el contenido de una cadena a otra - Funcion stcpy()*/
+/*Pedir al usuario una cadena de la caracteres, almacenarla en un arreglo
+y copiar todo su contenido hacia otro arreglo de caracteres*/
 #include <iostream>
 #include <conio.h>
 #include <string.h>
@@ -6,13 +7,14 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-  char nombre[] = "Cristian";
-  char nombre2[20];
+  char nombreUsuario[30];
+  char copiaNombre[30];
+
+  cout << "Por favor escribe tu nombre: ";
+  cin.getline(nombreUsuario, 30);
 
   // Copiamos los valores de la cadena con elementos a la cadena vacia
-  strcpy(nombre2, nombre);
-
-  cout << nombre2 << endl;
-
+  strcpy(copiaNombre, nombreUsuario);
+  cout << "Se realizo una copia de tu nombre en otro arreglo: " << copiaNombre;
   return 0;
 }
