f368d461c0f707d5884ccdda746eb553bf308464
Cristian Arraya <arrayacristian75@gmail.com>
2024-05-27 08:35:29 -0300

    Se uso la funciom strcat para concatenar una cadena con otra

--- hello.cpp
+++ hello.cpp
@@ -1,19 +1,20 @@
-/*Comparar cadenas - Funcion strcmp()*/
+/*Agregar o cancatenar una cadena con otra - Funcion strcat()*/
 #include <iostream>
 #include <conio.h>
 #include <string.h>
 using namespace std;
 
-//avion - becerro
-
 int main(int argc, char const *argv[])
 {
-  char palabra1[] = "becerro";
-  char palabra2[] = "avion";
+  char cad1[] = "Esto es una cadena";
+  char cad2[] = " de ejemplo";
+  char cad3[30];
 
-  if (strcmp(palabra1, palabra2) > 0)
-  {
-    cout << palabra1 << " Esta alfabeticamente";
-  }
+  strcpy(cad3, cad1); // cad3 = 'esto es una cadena'
+  strcat(cad3, cad2); // cad 3 = 'esto es una cadena de ejemplo'
+
+  cout << cad3 << endl;
+
+  getch();
   return 0;
 }
