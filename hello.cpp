e5ec186ca1d72a26c5c6b39065f6448cfe1f70d7
Cristian Arraya <arrayacristian75@gmail.com>
2024-05-27 08:56:02 -0300

    Se uso una funcion algo curiosa y divertida llamada strrev que invierte el orden de los caracteres

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,4 @@
-/*Crear una cadena que tenga la siguiente frase "Hola que tal", luego crear otra cadena para
-preguntarle
-al usuario su nombre, por ultimo agregar el nombre final de la primera cadena y
-mostrar el mensaje completo
-"Hola que tal (Nombre del usuario)"*/
+/*Invertir una cadena - Funcion strrev()*/
 #include <iostream>
 #include <conio.h>
 #include <string.h>
@@ -10,15 +6,9 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-  /*Variables que usaremos*/
-  char pregunta[] = "Como te llamas?"; // Preguntar al usuario su nombre
-  char nombreUsuario[30];// Guardamos el nombre en esta cadena
-  char saludo[] = "Hola que tal ";// Saludo al usuario
+  char cadena1[] = "Messi";
 
-  cout << pregunta << endl;
-  cin.getline(nombreUsuario, 30);//Nos servira para almacenar el nombre del usuario en la variable nombreUsario que tiene un limite de 30 espacio
-  cout << strcat(saludo, nombreUsuario) << endl;//Mostramos el mensaje del saludo + el nombre 
-
-  getch();
+  strrev(cadena1);
+  cout << cadena1 << endl;
   return 0;
 }
