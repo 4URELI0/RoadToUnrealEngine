11d8f5cda9978e4d39ce1b3f0b70bcc5d695a382
Cristian Arraya <arrayacristian75@gmail.com>
2024-05-27 09:25:04 -0300

    Se uso una funcion llamada strlwr que transforma la palabras mayusculas a minusculas

--- hello.cpp
+++ hello.cpp
@@ -1,4 +1,4 @@
-/*Convertir dos cadena de caracteres de minuscula a mayuscula. Compararlas y decir si son iguales o no*/
+/*Pasar una palabra a minuscula - Funcion strlwr()*/
 #include <iostream>
 #include <conio.h>
 #include <string.h>
@@ -6,17 +6,8 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-  char nombre1[] = "cristian";
-  char nombre2[] = "cristian";
-  strupr(nombre1);
-  strupr(nombre2);
-  if (strcmp(nombre1, nombre2) == 0)
-  {
-    cout << "Las palabras " << nombre1 << " y " << nombre2 << " son iguales";
-  }
-  else
-  {
-    cout << "Las palabras" << nombre1 << " y " << nombre2 << " no son iguales";
-  }
+  char palabra[] = "CRISTIAN";
+  strlwr(palabra);//Transforma la palabra a minuscula
+  cout << palabra << endl;
   return 0;
 }
