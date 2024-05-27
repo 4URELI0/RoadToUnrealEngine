9037017719521c7457ab59827ba8ea38b57c567c
Cristian Arraya <arrayacristian75@gmail.com>
2024-05-27 09:20:23 -0300

    Se compara si las palabras son iguales o no y saltara un mensaje

--- hello.cpp
+++ hello.cpp
@@ -1,4 +1,4 @@
-/*Pasar una palabra a Mayuscula - Funcion stupr()*/
+/*Convertir dos cadena de caracteres de minuscula a mayuscula. Compararlas y decir si son iguales o no*/
 #include <iostream>
 #include <conio.h>
 #include <string.h>
@@ -7,7 +7,16 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
   char nombre1[] = "cristian";
+  char nombre2[] = "cristian";
   strupr(nombre1);
-  cout << nombre1 << endl;
+  strupr(nombre2);
+  if (strcmp(nombre1, nombre2) == 0)
+  {
+    cout << "Las palabras " << nombre1 << " y " << nombre2 << " son iguales";
+  }
+  else
+  {
+    cout << "Las palabras" << nombre1 << " y " << nombre2 << " no son iguales";
+  }
   return 0;
 }
