9ccea584099e9ac9c9d46f20adb39880e409b089
Cristian Arraya <arrayacristian75@gmail.com>
2024-05-27 08:51:31 -0300

    Se realizo un programa que saluda al usuario despues que este ingresara su nombre

--- hello.cpp
+++ hello.cpp
@@ -1,4 +1,8 @@
-/*Agregar o cancatenar una cadena con otra - Funcion strcat()*/
+/*Crear una cadena que tenga la siguiente frase "Hola que tal", luego crear otra cadena para
+preguntarle
+al usuario su nombre, por ultimo agregar el nombre final de la primera cadena y
+mostrar el mensaje completo
+"Hola que tal (Nombre del usuario)"*/
 #include <iostream>
 #include <conio.h>
 #include <string.h>
@@ -6,14 +10,14 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-  char cad1[] = "Esto es una cadena";
-  char cad2[] = " de ejemplo";
-  char cad3[30];
+  /*Variables que usaremos*/
+  char pregunta[] = "Como te llamas?"; // Preguntar al usuario su nombre
+  char nombreUsuario[30];// Guardamos el nombre en esta cadena
+  char saludo[] = "Hola que tal ";// Saludo al usuario
 
-  strcpy(cad3, cad1); // cad3 = 'esto es una cadena'
-  strcat(cad3, cad2); // cad 3 = 'esto es una cadena de ejemplo'
-
-  cout << cad3 << endl;
+  cout << pregunta << endl;
+  cin.getline(nombreUsuario, 30);//Nos servira para almacenar el nombre del usuario en la variable nombreUsario que tiene un limite de 30 espacio
+  cout << strcat(saludo, nombreUsuario) << endl;//Mostramos el mensaje del saludo + el nombre 
 
   getch();
   return 0;
