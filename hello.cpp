0557af9fda6940ceda4c1691dddcaeade38fa179
Cristian Arraya <arrayacristian75@gmail.com>
2024-06-17 15:38:03 -0300

    Se aprendio realizo un nuevo tipo de vector y agregar un espacio al vector

--- hello.cpp
+++ hello.cpp
@@ -1,20 +1,35 @@
 #include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    // definicion de un vector estatico
-    // tipo-dato nombre-vector[tamanio]
+    // definicion de un vector dinamico
+    /*Formas de definir el vector dinamico*/
+    // 1- vector<tipo-dato> nombre Vector - sin tamanio
+    // 2- vector<tipo-dato> nombre- Vector(tamanio) - vector indicando su tamanio, se puede agregar mas si es que lo necesito
+    // 3- vector<tipo-dato> nombre-Vector(tamanio, valorInicial)- le indicamos su tamanio y por defecto que todas las celdas tengan por defecto x valor
     int n;
     cin >> n;
-    int ve[n];
+    vector<int> v(n);
     for (int i = 0; i < n; i++)
     {
-        cin >> ve[i];
+        cin >> v[i];
     }
     cout << endl;
     for (int i = 0; i < n; i++)
     {
-        cout << ve[i] << " ";
+        cout << "[" << v[i] << "]";
+    }
+
+    cout << endl << "Agregados" << endl;
+    int nuevoValor;
+    cin >> nuevoValor;
+    v.push_back(nuevoValor);
+    cout << endl;
+
+    for (int i = 0; i < v.size(); i++)//el size nos mostrara el tamanio real del vector
+    {
+        cout << "[" << v[i] << "]";
     }
 }
