53dfd3de96dfdc748283d1c8f32111fc239a398b
Cristian Arraya <arrayacristian75@gmail.com>
2024-06-17 13:25:32 -0300

    Se conocio un nuevo tipo de dato llamado long long que tiene un mayor almacenamiento

--- hello.cpp
+++ hello.cpp
@@ -1,32 +1,12 @@
-/*Realiza un programa que calcule el x1 y x2 de una ecuacion cuadratica y mostrar su valor por pantalla*/
 #include <iostream>
-#include <conio.h>
-#include <string.h>
-#include <math.h>
-#include <iomanip>
 using namespace std;
 
-int main(int argc, char const *argv[])
+int main()
 {
-  float terminoCuadratico;    // a
-  float terminoLineal;        // b
-  float terminoIndependiente; // c
+    long long n, suma;
+    cin >> n;
 
-  /*Muestra el mensaje y le solicita al usuario los datos e ingresa los datos*/
-  cout << "Por favor escribe el termino cuadratico: ";
-  cin >> terminoCuadratico;
-  cout << "Por favor escribe el termino lineal: ";
-  cin >> terminoLineal;
-  cout << "Escribe el termino independiente: ";
-  cin >> terminoIndependiente;
+    suma = (n * (n + 1)) / 2;
 
-  /*Formula de la ecuaciones*/
-  float x1 = (-terminoLineal + sqrt(pow(terminoLineal, 2) - 4 * terminoCuadratico * terminoIndependiente)) / (2 * terminoCuadratico);
-  float x2 = (-terminoLineal - sqrt(pow(terminoLineal, 2) - 4 * terminoCuadratico * terminoIndependiente)) / (2 * terminoCuadratico);
-  
-  /*Resultado por pantalla*/
-  cout << "Resultado de X1: " << fixed << setprecision(2) << x1 << endl;
-  cout << "Resultado de X2: " << fixed << setprecision(2) << x2 << endl;
-  
-  return 0;
+    cout << suma;
 }
